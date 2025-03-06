065c13347fa58221ef49103a6d5bafc28e6d3e03
Sanyam Gandhi <contact@sanyamseac.rf.gd>
2025-03-06 11:07:11 +0530

    Update theme.js

--- scripts/theme.js
+++ scripts/theme.js
@@ -22,3 +22,19 @@ const ThemeManager = {
         }
     }
 };
+
+
+// Initialize theme when DOM is loaded
+document.addEventListener('DOMContentLoaded', () => {
+    ThemeManager.init();
+});
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('../service-worker.js').then((registration) => {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }).catch((error) => {
+      console.log('ServiceWorker registration failed: ', error);
+    });
+  });
+}
